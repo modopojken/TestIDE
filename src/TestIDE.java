f1da8763f203fec35dc634570e7b2d2892e7e2bd
Elias Conradsson <coel16@nti.umea>
2017-11-06 10:55:53 +0100

    hela j*vla koden

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -4,6 +4,41 @@ public class TestIDE {
     public static void main(String[] args) {
 
 
+        int maximum;
+        int minimum;
+        int randomNum;
 
-}
+        maximum = 100;
+        minimum = 1;
+        String mittnr;
+
+        randomNum = minimum + (int) (Math.random()*maximum);
+        //minimum + (int)(Math.random() * maximum);
+
+
+    int mittnum = 0;
+    int ggr = 0;
+        while(ggr<13){
+
+            mittnr = JOptionPane.showInputDialog(null, "Skriv in ett nummer mellan ett och hundra din noob bich blaat");
+
+            mittnum = Integer.parseInt(mittnr);
+
+            if (mittnum == randomNum) {
+
+                JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+                System.exit(0);
+
+
+            }else if(mittnum < randomNum){
+                JOptionPane.showMessageDialog(null, "För litet, gissa på ett högre nummer");
+
+            }else if(mittnum > randomNum){
+
+                JOptionPane.showMessageDialog(null, "För stort, gissa på ett mindre nummer");
+            }
+            ggr++;
+
+        }
+    }
 }
